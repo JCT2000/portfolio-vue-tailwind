cda3017f0b04a240cc2ce34bd4fece37e5e6bc50
John Christian Triana <trianajohnchristian@gmail.com>
2024-01-31 16:14:00 +0800

    Dynamic import applied

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router"
-import Home from '@/components/Home.vue'
-import About from '@/components/About.vue'
-import Skills from '@/components/Skills.vue'
-import Project from '@/components/Project.vue'
-import Contact from '@/components/Contact.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+const Home = () => import('@/components/Home.vue');
+const About = () => import('@/components/About.vue');
+const Skills = () => import('@/components/Skills.vue');
+const Project = () => import('@/components/Project.vue');
+const Contact = () => import('@/components/Contact.vue');
 
 const routes =  [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: About },
   { path: "/skills", name: "Skills", component: Skills },
   { path: "/project", name: "Project", component: Project },
-  { path: "/contact", name: "Constact", component: Contact },
+  { path: "/contact", name: "Contact", component: Contact },
 ];
 
 const router = createRouter({
@@ -19,4 +20,28 @@ const router = createRouter({
   routes
 });
 
-export default router
+export default router;
+
+
+// import { createRouter, createWebHistory } from "vue-router"
+// import Home from '@/components/Home.vue'
+// import About from '@/components/About.vue'
+// import Skills from '@/components/Skills.vue'
+// import Project from '@/components/Project.vue'
+// import Contact from '@/components/Contact.vue'
+
+// const routes =  [
+//   { path: "/", name: "Home", component: Home },
+//   { path: "/about", name: "About", component: About },
+//   { path: "/skills", name: "Skills", component: Skills },
+//   { path: "/project", name: "Project", component: Project },
+//   { path: "/contact", name: "Constact", component: Contact },
+// ];
+
+// const router = createRouter({
+//   mode: 'hash',
+//   history: createWebHistory('/portfolio-vue-tailwind/'),
+//   routes
+// });
+
+// export default router
