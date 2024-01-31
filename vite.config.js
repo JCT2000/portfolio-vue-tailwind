65416d5cfcb8cc10ca349cfc4b0efb1236257fb6
John Christian Triana <trianajohnchristian@gmail.com>
2024-01-31 14:47:41 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import Vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [Vue()],
+  base: "/portfolio-vue-tailwind/",
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
-  }
+  },
 });
